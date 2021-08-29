035963e6128f30ec2b7ce447b8aeed337b71524f
Shivam <shiv.rawat594@gmail.com>
2021-08-30 01:16:43 +0530

    added edit feature

--- ASSIGNMENTS/Assignment-6/app.js
+++ ASSIGNMENTS/Assignment-6/app.js
@@ -5,14 +5,14 @@ const lis = document.querySelector('ul');
 btn.addEventListener('click', (e)=>{
     // console.log(inp.value)
     if(inp.value === ""){
-        alert("Invalid Task");
+        console.log("Empty String");
     }
     else{
         lis.innerHTML += `<li>${inp.value}</li>`;
-        lis.lastChild.innerHTML += `<i class="fas fa-pen edit" id="edit${lis.children.length} aria-hidden="true"></i>`;
-        lis.lastChild.innerHTML += `<i class="fas fa-trash-alt trash" id="trash${lis.children.length}" onclick="trashIt(this)" aria-hidden="true"></i>`;
-        lis.lastChild.innerHTML += `<i class="fa fa-angle-double-up up"  id="up${lis.children.length}" onclick="moveUp(this)" aria-hidden="true"></i>`;
-        lis.lastChild.innerHTML += `<i class="fa fa-angle-double-down down" id="down${lis.children.length}" onclick="moveDown(this)" aria-hidden="true"></i>`;
+        lis.lastChild.innerHTML += ` <i class="fas fa-pen edit" onclick="editIt(this)" aria-hidden="true"></i>`;
+        lis.lastChild.innerHTML += ` <i class="fas fa-trash-alt trash" onclick="trashIt(this)" aria-hidden="true"></i>`;
+        lis.lastChild.innerHTML += ` <i class="fa fa-angle-double-up up" onclick="moveUp(this)" aria-hidden="true"></i>`;
+        lis.lastChild.innerHTML += ` <i class="fa fa-angle-double-down down" onclick="moveDown(this)" aria-hidden="true"></i>`;
         inp.value = "";
     }
 })
@@ -44,3 +44,14 @@ function trashIt(node){
     node.parentElement.remove();
 }
 
+function editIt(node){
+    // console.log(node);
+    // console.log(node.parentElement.innerText);
+    var val = prompt("Change text");
+    // console.log(val);
+    if(val != ""){
+    var x = `${val} <i class="fas fa-pen edit" onclick="editIt(this)" aria-hidden="true"></i> <i class="fas fa-trash-alt trash" onclick="trashIt(this)" aria-hidden="true"></i> <i class="fa fa-angle-double-up up" onclick="moveUp(this)" aria-hidden="true"></i> <i class="fa fa-angle-double-down down" onclick="moveDown(this)" aria-hidden="true"></i>`;
+    node.parentElement.innerHTML = x; 
+    }
+    // node.parentElement.innerHTML = ;
+}
