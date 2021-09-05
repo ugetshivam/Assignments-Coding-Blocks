f71009677ddb560e279d7d5df8b187021e505a25
Shivam <shiv.rawat594@gmail.com>
2021-09-05 16:19:59 +0530

    Assignment changes

--- ASSIGNMENTS/Assignment-7/app.js
+++ ASSIGNMENTS/Assignment-7/app.js
@@ -7,7 +7,34 @@ const dttime = document.getElementById('dt-time');
 
 
 var date = new Date();
-
+function backgroundChange(weather) {
+    var imageUrl = "https://www.onhisowntrip.com/wp-content/uploads/2021/04/z4.jpg";
+    if(weather === 'mist'){
+        imageUrl= "https://images.unsplash.com/photo-1487621167305-5d248087c724?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWlzdHklMjBmb3Jlc3R8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80";
+        
+    }
+    else if(weather.includes('clear')){
+        imageUrl = "https://images.unsplash.com/photo-1546440730-4716c1a47815?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=719&q=80";
+    }
+    else if(weather === 'haze'){
+        imageUrl= "https://images.unsplash.com/photo-1594492215832-a299f28a00c1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+    }
+    else if(weather.includes('rain')){
+        imageUrl = "https://images.unsplash.com/photo-1523772721666-22ad3c3b6f90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+    }
+    else if(weather.includes('cloud')){
+        imageUrl = "https://images.unsplash.com/photo-1517828787276-cf8cb82c87ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=667&q=80";
+    }
+    else if(weather.includes('storm')){
+        imageUrl = "https://images.unsplash.com/photo-1488812690953-601000f207e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=767&q=80";
+    }
+    else if(weather.includes('snow')){
+        imageUrl = "https://images.unsplash.com/photo-1595391595208-204a890b02e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+    }
+    $('#container').css("background-image", "url(" + imageUrl + ")");
+        $('#container').css("background-repeat", "no-repeat");
+        $('#container').css("background-size", "cover");
+  }
 fetch('https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=7c2a64da9f45a1423043dde66bd600dd')
 .then((res)=>{
     return res.json();
@@ -20,6 +47,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appi
     temp.innerHTML = data.main.temp + "&#8451;" + "&nbsp;&nbsp;"+ f.toPrecision(4) + "&deg;F";
     desc.innerHTML = data.weather[0].description;
     icon.src= "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    backgroundChange(data.weather[0].description);
 })
 .catch((err)=>{
     console.log(err.message);
@@ -53,6 +81,7 @@ dttime.innerHTML = time;
         icon.src= "";
     }
     inp.value = "";
+    backgroundChange(data.weather[0].description);
 })
 .catch((err)=>{
     console.log(err.message);
